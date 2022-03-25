46775746fd6a273749472fed906f6f28cf6cad87
mSkarbinska <mskarbinska@student.agh.edu.pl>
2022-03-25 01:00:08 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import PokemonCard from './components/PokemonCard';
+import * as ReactBootStrap from 'react-bootstrap'
+import './App.css'
 
 function App() {
   const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
   const [pokemonsData, setPokemonsData] = useState([])
-  
+  const [loading, setLoading] = useState(false)
+
   useEffect(() => {
     getPokemons()
   }, [])
@@ -30,22 +33,36 @@ function App() {
       const data =  await res.json()
       setPokemonsData( pokemonsData => [...pokemonsData, data])
     })
+    setLoading(true)
   }
 
   return (
-    <div>
-    <div>
-      {pokemonsData.map((p, index)=>(
-          <PokemonCard 
-              key = {index} 
-              name={p.name} 
-              id = {index} 
-              img = {p.sprites.other.dream_world.front_default} type={p.types[0].type.name}> 
-          </PokemonCard>
-      ))}
+    <div className="col-xs-1" align="center">
+    <ReactBootStrap.Navbar className="justify-content-center">
+      <img  src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"></img>
+    </ReactBootStrap.Navbar>
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-2">
+        {pokemonsData.map((p, index)=>(
+            <PokemonCard  
+                key = {index} 
+                name={p.name} 
+                id = {index} 
+                img = {p.sprites.other.dream_world.front_default} type={p.types[0].type.name}> 
+            </PokemonCard>
+        ))}
+      </div>
+      
     </div>
-    <button onClick={() => getPokemons()}>Load more pokemons</button>
-    </div>
+
+    { !loading ? <ReactBootStrap.Spinner animation="border"/>:
+    <ReactBootStrap.Button variant="primary"  style = {{margin: '2rem'}} onClick={() =>{
+      getPokemons() 
+      setLoading(false)}}>Load more</ReactBootStrap.Button>}
+    
+    
+  </div>
+    
   );
 }
 
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import {Card, Button} from 'react-bootstrap'
 
 export default function PokemonCard({name, type, img, id}) {
-  return (
-      <div>
-            {name}
-            {id}
-            <img alt={name} src ={img} />
-            {type}
-      </div>
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+  
+    return (
+      <Card className="text-center" style={{ width: '18rem', margin: '1rem'}}>
+        <Card.Img variant="top" src={img} style={{ padding:'2rem', width:'18rem', height:'18rem'}} />
+        <Card.Body>
+        <Card.Title>{capitalizeFirstLetter(name)}</Card.Title>
+        <Card.Text>
+            {capitalizeFirstLetter(type)}
+        </Card.Text>
+        <Button variant="primary">More info</Button>
+        </Card.Body>
+      </Card>
 
   )
 }
